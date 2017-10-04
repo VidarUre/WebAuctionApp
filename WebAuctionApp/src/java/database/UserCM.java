cb2761c1cf391d60a7e2cc6b31e169c04a7f9e13
VidarUre <vidarure@gmail.com>
2017-10-04 08:38:50 +0200

    Add ALL the things!

---
+++ WebAuctionApp/src/java/database/UserCM.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package database;
+
+import beans.User;
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Vidar
+ */
+@Stateless
+public class UserCM {
+
+    @PersistenceContext(unitName = "EnterpriseApplication77PU")
+    private EntityManager em;
+    
+    protected EntityManager getEntityManager() {
+        return em;
+   }
+    
+    public void storeUser(User user) {
+        em.persist(user);
+    }
+    
+    public User findUser(String username) {
+        User foundUser = em.find(User.class, username);
+        return foundUser;
+    }
+    
+    public boolean isValidLogin(String username, String password) {
+       return true;
+    }
+}
